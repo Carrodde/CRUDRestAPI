f2f9606316aa062c2ffc9071a92d6e424a7560ea
Carolina Ekdal <Carolina.ekdal@gmail.com>
2024-03-06 14:33:37 +0100

    created function to add player

--- controllers/playerController.js
+++ controllers/playerController.js
@@ -1,1 +1,24 @@
 const { player } = require("../models");
+
+async function addPlayer(req, res) {
+  const { name, jersey, position } = req.body;
+  const existingPlayer = await player.findOne({
+    where: {
+      name: req.body.name,
+    },
+  });
+  if (existingPlayer) {
+    return res.status(401).send("Player already exist");
+  } else {
+    await player.create({
+      name: name,
+      jersey: jersey,
+      position: position,
+    });
+  }
+  return res.status(201).send("Player added!");
+}
+
+module.exports = {
+  addPlayer,
+};
